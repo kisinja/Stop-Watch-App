6c0cd75510ecb52e27499098321890040e0db09c
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-06-08 21:14:38 +0300

    Changes to be committed: 	modified:   src/StopWatch.jsx 	modified:   src/index.css

--- src/StopWatch.jsx
+++ src/StopWatch.jsx
@@ -54,9 +54,22 @@ const StopWatch = () => {
                 {formatTime()}
             </div>
             <div className="controls">
-                <button onClick={start} className="start-btn">Start</button>
-                <button onClick={stop} className="stop-btn">Stop</button>
-                <button onClick={reset} className="reset-btn">Reset</button>
+                <button onClick={start} className="start-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-play">
+                        <polygon points="5 3 19 12 5 21 5 3"></polygon>
+                    </svg>
+
+                </button>
+                <button onClick={stop} className="stop-btn">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-square">
+                        <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
+                    </svg>
+
+                </button>
+
+                <button onClick={reset} className="reset-btn">
+                    Reset
+                </button>
             </div>
         </div>
     );
